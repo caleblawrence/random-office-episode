fd16d96d939e98bc6c9f444cc2de4a0c5131b3c6
Caleb Lawrence <lawrence.calebc@gmail.com>
2020-03-25 22:07:03 -0500

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Home = () => {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Office Random Episode</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
